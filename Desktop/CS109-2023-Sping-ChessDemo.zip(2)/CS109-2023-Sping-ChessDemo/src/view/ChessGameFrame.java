b9ab61f69dff2cdf68780f07f5b3009c7bb510ea
cyy1019 <12210260@mail.sustech.edu.cn>
2023-05-15 16:28:26 +0800

    fixed

---
+++ Desktop/CS109-2023-Sping-ChessDemo.zip(2)/CS109-2023-Sping-ChessDemo/src/view/ChessGameFrame.java
@@ -0,0 +1,113 @@
+package view;
+
+import controller.GameController;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * 这个类表示游戏过程中的整个游戏界面，是一切的载体
+ */
+public class ChessGameFrame extends JFrame {
+    //    public final Dimension FRAME_SIZE ;
+    private final int WIDTH;
+    private final int HEIGTH;
+
+    private final int ONE_CHESS_SIZE;
+
+    private ChessboardComponent chessboardComponent;
+    private GameController gameController;
+
+    public void setGameController(GameController gameController) {
+        this.gameController = gameController;
+    }
+
+    public ChessGameFrame(int width, int height) {
+        setTitle("2023 CS109 Project Demo"); //设置标题
+        this.WIDTH = width;
+        this.HEIGTH = height;
+        this.ONE_CHESS_SIZE = (HEIGTH * 4 / 5) / 9;
+
+        setSize(WIDTH, HEIGTH);
+        setLocationRelativeTo(null); // Center the window.
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //设置程序关闭按键，如果点击右上方的叉就游戏全部关闭了
+        setLayout(null);
+
+
+        addChessboard();
+        addLabel();
+        addHelloButton();
+        addLoadButton();
+        savejButtonActionPerformed();
+    }
+
+    public ChessboardComponent getChessboardComponent() {
+        return chessboardComponent;
+    }
+
+    public void setChessboardComponent(ChessboardComponent chessboardComponent) {
+        this.chessboardComponent = chessboardComponent;
+    }
+
+    /**
+     * 在游戏面板中添加棋盘
+     */
+    private void addChessboard() {
+        chessboardComponent = new ChessboardComponent(ONE_CHESS_SIZE);
+        chessboardComponent.setLocation(HEIGTH / 5, HEIGTH / 10);
+        add(chessboardComponent);
+    }
+
+    /**
+     * 在游戏面板中添加标签
+     */
+    private void addLabel() {
+        JLabel statusLabel = new JLabel("Sample label");
+        statusLabel.setLocation(HEIGTH, HEIGTH / 10);
+        statusLabel.setSize(200, 60);
+        statusLabel.setFont(new Font("Rockwell", Font.BOLD, 20));
+        add(statusLabel);
+    }
+
+    /**
+     * 在游戏面板中增加一个按钮，如果按下的话就会显示Hello, world!
+     */
+
+    private void addHelloButton() {
+        JButton button = new JButton("Show Hello Here");
+        button.addActionListener((e) -> JOptionPane.showMessageDialog(this, "Hello, world!"));
+        button.setLocation(HEIGTH, HEIGTH / 10 + 120);
+        button.setSize(200, 60);
+        button.setFont(new Font("Rockwell", Font.BOLD, 20));
+        add(button);
+    }
+
+    private void addLoadButton() {
+        JButton button = new JButton("Load");
+        button.setLocation(HEIGTH, HEIGTH / 10 + 240);
+        button.setSize(200, 60);
+        button.setFont(new Font("Rockwell", Font.BOLD, 20));
+        add(button);
+//通过点击当前button可触发的事件都写到括号内
+        button.addActionListener(e -> {// 直接在按钮这里写监听器
+            System.out.println("Click load");
+            //JOptionPane提供用户输入与程序交互的接口
+            String path = JOptionPane.showInputDialog(this, "Input Path here");
+            //输入框写入数据都会存在该String类型数据里
+            gameController.loadGameFromFile(path);
+            System.out.println(path);
+        });
+    }
+
+    //保存
+    private void savejButtonActionPerformed() {
+        JButton savejButton = new JButton("Save");
+        savejButton.setLocation(HEIGTH, HEIGTH / 10 + 360);
+        savejButton.setSize(200, 60);
+        savejButton.setFont(new Font("Rockwell", Font.BOLD, 20));
+        add(savejButton);
+
+    }
+}
